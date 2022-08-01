1e68584f2450521f223f8bd5b0d59f745ddf29af
gggdbstjs <106298143+gggdbstjs@users.noreply.github.com>
2022-08-01 10:47:27 +0900

    Delete common.js

--- common.js
+++
@@ -1,9 +0,0 @@
-$('header').load('./inc.html header > div');
-$('footer').load('./inc.html footer > div');
-
-$('.search_btn_header').on('click',function(){
-    $('.search').addClass('active');
-})
-$('.close').on('click',function(){
-    $('.search').removeClass('active');
-})
