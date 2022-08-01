a8b4135c0fbb3353676acb232ea017367d813d63
gggdbstjs <106298143+gggdbstjs@users.noreply.github.com>
2022-08-01 10:48:32 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,2 +1,9 @@
 $('header').load('./inc.html header > div');
 $('footer').load('./inc.html footer > div');
+
+$('.search_btn_header').on('click',function(){
+    $('.search').addClass('active');
+})
+$('.close').on('click',function(){
+    $('.search').removeClass('active');
+})
